b4b21bf6e52813cac5e9dc30eb3f95cd2743d717
Waffles <knharman@gmail.com>
2021-12-17 08:56:17 -0800

    3 cards displaying

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,64 +1,86 @@
 var getWeatherData = function() {
     var appId = "e0c1ba6410238103fe2d482d8b1d932f"
-    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=45.52&lon=-122.67&appid=${appId}`;
+    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=45.52&lon=-122.67&appid=${appId}&units=imperial`;
     
     fetch(url)
         .then(function (response) {
             return response.json();
         })
         .then(function (responseData) {
-            // displayWeatherData(responseData)
-            console.log(responseData);
             return responseData;
         })
+        .then(function(weatherData) {
+            displayWeatherData(weatherData);
+        })
         .catch(function (error) {
-            alert("Unable to fetch weather data.")
+            console.log(error);
+            alert("Unable to fetch weather data.");
         })
 }
 
-var displayWeatherData = function(data) {
-    var weatherWrapper = document.getElementById("weatherWrapper")
-    for(i=1; i<=3; i++) {
-        var singleCard = singleWeatherCard(data.daily[i])
-        weatherWrapper.appendChild(singleCard)
+var displayWeatherData = function(weatherData) {
+    console.log(weatherData);
+    // var weatherWrapper = document.getElementById("weatherWrapper")
+    // weatherWrapper.innerHTML = "";
+
+    // var cardContainer = document.createElement("div");
+    // cardContainer.className = "row d-flex justify-content-around";
+
+    // container.appendChild(cardContainer);
+
+    for (i=1; i<4; i++) {
+        var card = singleWeatherCard(weatherData.daily[i]);
+        weatherWrapper.appendChild(card);
     }
-    
 }
 
-// var singleWeatherCard = function (weatherData) {
-//     var card = document.createElement("div");
-//     card.className = "card text-center bg-dark text-white";
+var singleWeatherCard = function (weatherData) {
+    console.log(weatherData)
+    var card = document.createElement("div");
+    card.className = "card text-center bg-dark text-white";
 
-//     var cardBody = document.createElement("div");
-//     cardBody.className = "card-body";
+    var cardBody = document.createElement("div");
+    cardBody.className = "card-body";
 
-//     var date = document.createElement("h4");
-//     date.innerHTML = moment.unix(weatherData.dt).format("MM/DD/YYYY");
-//     date.className = "card-title";
+    var date = document.createElement("h4");
+    date.innerHTML = moment.unix(weatherData.dt).format("MM/DD/YYYY");
+    date.className = "card-title";
 
-//     var icon = document.createElement("img");
-//     icon.src = weatherIconURL(weatherData.weather[0].icon);
+    var icon = document.createElement("img");
+    icon.src = weatherIconURL(weatherData.weather[0].icon);
 
-//     var temp = document.createElement("h6");
-//     var tempAverage = getTempAverage(weatherData.temp);
-//     temp.innerHTML = `Temp: ${tempAverage.toFixed(2)}&#xb0;F`;
+    var temp = document.createElement("h6");
+    var tempAverage = getTempAverage(weatherData.temp);
+    temp.innerHTML = `Temp: ${tempAverage.toFixed(2)}&#xb0;F`;
 
-//     var wind = document.createElement("h6");
-//     wind.innerHTML = `Wind: ${weatherData.wind_speed} MPH`;
+    var wind = document.createElement("h6");
+    wind.innerHTML = `Wind: ${weatherData.wind_speed} MPH`;
 
-//     var humidity = document.createElement("h6");
-//     humidity.innerHTML = `Humidity: ${weatherData.humidity}%`;
+    var humidity = document.createElement("h6");
+    humidity.innerHTML = `Humidity: ${weatherData.humidity}%`;
 
-//     cardBody.appendChild(date);
-//     cardBody.appendChild(icon);
-//     cardBody.appendChild(temp);
-//     cardBody.appendChild(wind);
-//     cardBody.appendChild(humidity);
+    cardBody.appendChild(date);
+    cardBody.appendChild(icon);
+    cardBody.appendChild(temp);
+    cardBody.appendChild(wind);
+    cardBody.appendChild(humidity);
 
-//     card.appendChild(cardBody);
+    card.appendChild(cardBody);
 
-//     return card;
-// }
+    return card;
+}
+
+var weatherIconURL = function(iconCode) {
+    // http://openweathermap.org/img/wn/01d.png
+    return `http://openweathermap.org/img/wn/${iconCode}.png`;
+}
+
+var getTempAverage = function(temp) {
+    var { day, eve, morn, night } = temp;
+    var sum = day + eve + morn + night;
+    return sum / 4;
+}
+
 
 getWeatherData();
 
